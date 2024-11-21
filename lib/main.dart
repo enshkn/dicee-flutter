811d0194c84f69cdedca96c250a2bef995cbd76c
Enes Hakan <96254574+enshkn@users.noreply.github.com>
2024-11-21 14:34:57 +0000

    my project has done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,18 +7,78 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePageSt(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePageStateful extends StatefulWidget {
+  const DicePageStateful({super.key});
+
+  @override
+  State<DicePageStateful> createState() => _DicePageStatefulState();
+}
+
+class _DicePageStatefulState extends State<DicePageStateful> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Placeholder();
   }
 }
+
+
+/* class DicePage extends StatelessWidget { } */
+
+class DicePageSt extends StatefulWidget {
+  const DicePageSt({super.key});
+
+  @override
+  State<DicePageSt> createState() => _DicePageStState();
+}
+
+class _DicePageStState extends State<DicePageSt> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1 ;
+      rightDiceNumber = Random().nextInt(6) + 1 ;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: MaterialButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      changeDiceNumber();
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png')),
+              )),
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: MaterialButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      changeDiceNumber();
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png')),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
